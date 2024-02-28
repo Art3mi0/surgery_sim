334351718767f8a95325b83aecb619c3205c5f2b
Art3mi0 <temomez123@gmail.com>
2024-02-28 15:46:04 -0500

    Overlay will show somethign, but not correct.

---
+++ src/switch_node_no_ff.cpp
@@ -0,0 +1,244 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/PoseStamped.h>
+//#include <omni_msgs/OmniButtonEvent.h>
+#include <surgery_sim/Plan.h>
+#include <surgery_sim/Reset.h>
+#include <cstdlib>
+#include<omni_msgs/OmniFeedback.h>
+#include "std_msgs/Int32.h"
+
+std_msgs::Int32 button_data;
+geometry_msgs::Twist plan_point;
+geometry_msgs::Twist haptic_point;
+geometry_msgs::Twist robot_point;
+bool plan_received = false;
+bool haptic_received = false;
+bool button_received = false;
+bool h_pose_received = false;
+bool robot_received = false;
+bool timer_white = false;
+bool timer_grey = false;
+bool update_flag;
+bool white_press;
+bool grey_press;
+int click_count = 0;
+
+void button_callback(const std_msgs::Int32 &  _data){
+	// read the button input
+	button_data = _data;
+	button_received = true;
+}
+
+void plan_callback(const geometry_msgs::Twist &  _data){
+	// read the generated plan
+	plan_point = _data;
+	plan_received = true;
+}
+
+void haptic_callback(const geometry_msgs::Twist &  _data){
+	// read the pose of the haptic device
+	haptic_point = _data;
+	haptic_received = true;
+}
+
+void robot_callback(const geometry_msgs::Twist &  _data){
+	// read the pose of the robot
+	robot_point = _data;
+	robot_received = true;
+}
+
+geometry_msgs::PoseStamped phantom_pos;
+
+void get_phantom_pos(const geometry_msgs::PoseStamped & _data){
+	phantom_pos = _data;
+	h_pose_received = true;
+}
+
+// desired values of x, y, z for centering the haptic device
+double x_d = -0.007;
+double y_d = -.053;
+double z_d = 0.07;
+
+// ry = hx divide r by 3 rz = hz r/3 -rx=hy
+
+// proportional and derivate control gains for the x,y,z axis of the haptic device when tracking a certain reference position of the robot or centering them
+double kp_x = 25.0;
+double kd_x = 20.0;
+double kp_y = 25.0;
+double kd_y = 20.0;
+double kp_z = 50.0;
+double kd_z = 20.0;
+
+
+double e_x_prev = 0.0, e_y_prev = 0.0, e_z_prev = 0.0;
+
+
+// current and previous centering forces for the haptic device
+omni_msgs::OmniFeedback centering_force;
+omni_msgs::OmniFeedback centering_force_prev;
+
+//PD tracker for the position of the haptic device
+void calc_center_force(void){
+	double e_x, e_y, e_z, de_x, de_y, de_z;
+	//calculate the error
+  e_x = x_d - phantom_pos.pose.position.x; 
+	e_y = y_d - phantom_pos.pose.position.y; 
+	e_z = z_d - phantom_pos.pose.position.z; 
+        //calculate the derivatives of the errors
+        de_x = e_x - e_x_prev;
+	de_y = e_y - e_y_prev;
+	de_z = e_z - e_z_prev;
+        //low pass filter for reducing the noise and jerks in the forces
+	double tau = .3;
+	centering_force.force.x = (kp_x*e_x + kd_x*de_x)*(1-tau) + tau*centering_force_prev.force.x;
+	centering_force.force.y = (kp_y*e_y + kd_y*de_y)*(1-tau) + tau*centering_force_prev.force.y;
+  centering_force.force.z = (kp_z*e_z + kd_z*de_z)*(1-tau) + tau*centering_force_prev.force.z + 0.2;//the last component is for the effect hand weight
+	// reduce the kicks by resetting th force feedbacks when the reference changes
+ 	e_x_prev = e_x;
+	e_y_prev = e_y;
+	e_z_prev = e_z;
+	centering_force_prev = centering_force;
+}
+
+void timer_callback(const ros::TimerEvent& event){
+	if (white_press){
+		timer_white = true;
+	} else{
+		timer_grey = true;
+	}
+}
+
+void update_force(double x, double y, double z){
+	x_d = y/3;
+	y_d = -x;
+	z_d = z/3;
+}
+
+int main(int argc, char* argv[]){
+  ros::init(argc, argv, "switch_node");
+  ros::NodeHandle node;
+  
+  // initializing server interaction
+  ros::ServiceClient traj_client = node.serviceClient<surgery_sim::Reset>("reset");
+  ros::ServiceClient haptic_client = node.serviceClient<surgery_sim::Reset>("haptic_server");
+  ros::ServiceClient frame_client = node.serviceClient<surgery_sim::Reset>("frame_server");
+  ros::ServiceClient plan_client = node.serviceClient<surgery_sim::Reset>("plan_server");
+  surgery_sim::Reset reset;
+  
+  ros::Timer timer = node.createTimer(ros::Duration(1), timer_callback, true);
+ 
+	// subscriber for reading haptic device button input
+	ros::Subscriber button_sub = node.subscribe("/key", 1, button_callback);
+	// Subscriber for reading the plan trajectory
+	ros::Subscriber plan_sub = node.subscribe("/refplan", 1, plan_callback);
+	// Subscriber for reading the haptic trajectory
+	ros::Subscriber haptic_sub = node.subscribe("/refhap", 1, haptic_callback);
+	// Subscriber for reaping the haptic device pose
+	ros::Subscriber haptic_pos_sub = node.subscribe("/phantom/phantom/pose",10, get_phantom_pos);
+	// Subscriber for reaping the robot pose
+	ros::Subscriber robot_pos_sub = node.subscribe("/ur5e/toolpose",10, robot_callback);
+	// Publisher for the kinematic solver. Skips the plan point generation
+  ros::Publisher pub_robot = node.advertise<geometry_msgs::Twist>("/reftraj", 1);
+  // Publisher for the force feedback of haptic device
+  ros::Publisher force_pub =node.advertise<omni_msgs::OmniFeedback>("/phantom/phantom/force_feedback",1);
+  
+  bool white_flag;
+  bool grey_flag;
+  bool robot_flag = true;
+  
+  double rx;
+	double ry;
+	double rz;
+  
+  geometry_msgs::Twist robot_initial;
+  
+  ros::Rate rate(10.0);
+
+  while (node.ok()){
+  	if (robot_received){
+  		if(robot_flag){
+				robot_initial = robot_point;
+				robot_flag = false;
+			}else{
+				rx = (robot_point.linear.x - robot_initial.linear.x)*.5;
+				ry = (robot_point.linear.y - robot_initial.linear.y);
+				rz = (robot_point.linear.z - robot_initial.linear.z);
+			}
+  	}
+  	
+  	//left-white middle-grey
+  	//Left pedal-a	Middle pedal-b Right pedal-c
+  	if (button_received){
+  		if (button_data.data == 97 && (click_count == 0 || white_flag)){
+  			white_flag = false;
+  			grey_flag = true;
+  			white_press = true;
+  			grey_press = false;
+  			timer_grey = false;
+  			reset.request.plan_start = true;
+  			reset.request.plan_flag = true;
+  			reset.request.hap_start = false;
+  			reset.request.hap_flag = false;
+  			haptic_client.call(reset);
+  			frame_client.call(reset);
+  			if (click_count > 0){
+  				plan_client.call(reset);
+  			} else{
+  				traj_client.call(reset);
+  			}
+    		ROS_INFO("out: started plan. haptic off");
+    		haptic_received = false;
+    		click_count ++;
+    		timer.stop();
+    		timer.setPeriod(ros::Duration(.2));
+    		timer.start();
+  		}
+  		else if (button_data.data == 98 && (click_count == 0 || grey_flag)){
+  			white_flag = true;
+  			grey_flag = false;
+  			white_press = false;
+  			grey_press = true;
+  			timer_white = false;
+  			reset.request.plan_flag = false;
+  			reset.request.plan_start = false;
+  			reset.request.hap_start = true;
+  			reset.request.hap_flag = true;
+  			traj_client.call(reset);
+  			plan_client.call(reset);
+  			haptic_client.call(reset);
+  			//frame_client.call(reset);
+    		ROS_INFO("out: resetting plan. haptic on");
+    		plan_received = false;
+    		click_count ++;
+    		timer.stop();
+    		timer.setPeriod(ros::Duration(.5));
+    		timer.start(); 		
+  		}
+  			
+  		if (timer_white && plan_received){
+  			//update_force(rx, ry, rz); /*
+  			pub_robot.publish(plan_point);
+			} else if (timer_grey && haptic_received){
+				pub_robot.publish(haptic_point);
+			}
+		}
+		//if (h_pose_received && robot_received){
+			// update_force(rx, ry, rz);
+			// calc_center_force();
+			// force_pub.publish(centering_force);
+			/*
+			ROS_INFO("Differences: x:%f y:%f z:%f", phantom_pos.pose.position.x - ry/3, phantom_pos.pose.position.y - -rx, phantom_pos.pose.position.z - rz/3);
+		}*/
+		
+		rate.sleep();
+		ros::spinOnce();
+  }
+  return 0;
+};
+/*
+reset.request.flag = true;
+  			if (client.call(reset))
+  			{
+    			ROS_INFO("out: %s", (bool)reset.response.out ? "true" : "false");
+  			} */
--- src/user_overlay.cpp
+++ src/user_overlay.cpp
@@ -1,26 +1,36 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
 
 static const std::string OPENCV_WINDOWL = "Image window left";
 static const std::string OPENCV_WINDOWR = "Image window right";
+static const int RADIUS = 10;
 cv_bridge::CvImagePtr cv_ptr_l;
 cv_bridge::CvImagePtr cv_ptr_r;
+image_geometry::PinholeCameraModel cam_model_l;
+image_geometry::PinholeCameraModel cam_model_r;
 bool flagL = false;
 bool flagR = false;
+bool pcl_received = false;
 std::string text = "Current Mode:";
+pcl::PointCloud<pcl::PointXYZ> plan_cloud;
 
-void imageCbL(const sensor_msgs::ImageConstPtr& msg)
+void imageCbL(const sensor_msgs::ImageConstPtr& image_msg_l,
+const sensor_msgs::CameraInfoConstPtr& info_msg_l)
 {
   try
   {
-    cv_ptr_l = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    cv_ptr_l = cv_bridge::toCvCopy(image_msg_l, sensor_msgs::image_encodings::BGR8);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -28,8 +38,9 @@ void imageCbL(const sensor_msgs::ImageConstPtr& msg)
     return;
   }
 
+  cam_model_l.fromCameraInfo(info_msg_l);
+
   // Draw an example circle on the video stream
-  if (cv_ptr_l->image.rows > 60 && cv_ptr_l->image.cols > 60)
     cv::putText(cv_ptr_l->image, 
     text,
     cv::Point(cv_ptr_l->image.cols/2, 50), 
@@ -45,11 +56,12 @@ void imageCbL(const sensor_msgs::ImageConstPtr& msg)
   flagL = true;
 }
 
-void imageCbR(const sensor_msgs::ImageConstPtr& msg)
+void imageCbR(const sensor_msgs::ImageConstPtr& image_msg_r,
+const sensor_msgs::CameraInfoConstPtr& info_msg_r)
 {
   try
   {
-    cv_ptr_r = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    cv_ptr_r = cv_bridge::toCvCopy(image_msg_r, sensor_msgs::image_encodings::BGR8);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -57,8 +69,9 @@ void imageCbR(const sensor_msgs::ImageConstPtr& msg)
     return;
   }
 
+  cam_model_r.fromCameraInfo(info_msg_r);
+
   // Draw an example circle on the video stream
-  if (cv_ptr_r->image.rows > 60 && cv_ptr_r->image.cols > 60)
     cv::putText(cv_ptr_r->image, 
     text,
     cv::Point(cv_ptr_r->image.cols/2, 50), 
@@ -74,21 +87,62 @@ void imageCbR(const sensor_msgs::ImageConstPtr& msg)
   flagR = true;
 }
 
+void pcl_callback(const sensor_msgs::PointCloud2 &  _data){
+	pcl::fromROSMsg(_data, plan_cloud);
+	pcl_received = true;
+}
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "user_overlay");
   ros::NodeHandle node;
   image_transport::ImageTransport it(node);
-  image_transport::Subscriber subL = it.subscribe("/stereo/left/image_raw", 1, imageCbL);
-  image_transport::Subscriber subR = it.subscribe("/stereo/right/image_raw", 1, imageCbR);
+
+  ros::Subscriber pcl_sub = node.subscribe("/plancloud", 1, pcl_callback);
+  // ros::Publisher pcl_pub = node.advertise<pcl::PointCloud<pcl::PointXYZ> >("/testcloud", 1);
+
+// should subscribe to image_rect_color when using real cameras. Need to run image_proc for this topic
+  image_transport::CameraSubscriber subL = it.subscribeCamera("/stereo/left/image_raw", 1, imageCbL);
+  image_transport::CameraSubscriber subR = it.subscribeCamera("/stereo/right/image_raw", 1, imageCbR);
   image_transport::Publisher pubL = it.advertise("/overlay_left/output_video", 1);
   image_transport::Publisher pubR = it.advertise("/overlay_right/output_video", 1);
 
+  tf::TransformListener listener;
+  pcl::PointCloud<pcl::PointXYZ>  cloud_out_l;
+  pcl::PointCloud<pcl::PointXYZ>  cloud_out_r;
   int loop_freq = 60;
   ros::Rate loop_rate(loop_freq);
   
   while (node.ok()){
+     if (pcl_received){ 
+      tf::StampedTransform transform;
+      geometry_msgs::Transform transform2;
+      try
+      {
+        pcl_ros::transformPointCloud(cam_model_l.tfFrame(), plan_cloud, cloud_out_l, listener);
+        pcl_ros::transformPointCloud(cam_model_r.tfFrame(), plan_cloud, cloud_out_r, listener);
+        for (int i = 0; i < cloud_out_l.points.size(); i++){
+          cv::Point3d pt_cv_l(cloud_out_l.points[i].x, cloud_out_l.points[i].y, cloud_out_l.points[i].z);
+          cv::Point3d pt_cv_r(cloud_out_r.points[i].x, cloud_out_r.points[i].y, cloud_out_r.points[i].z);
+          cv::Point2d uv_l;
+          cv::Point2d uv_r;
+          uv_l = cam_model_l.project3dToPixel(pt_cv_l);
+          uv_r = cam_model_r.project3dToPixel(pt_cv_r);
+
+          cv::circle(cv_ptr_l->image, uv_l, RADIUS, CV_RGB(255,0,0), -1);
+          cv::circle(cv_ptr_r->image, uv_r, RADIUS, CV_RGB(255,0,0), -1);
+        }
+      }
+      catch (tf::TransformException ex)
+      {
+        ROS_WARN("%s", ex.what());
+        ros::Duration(1.0).sleep();
+      }
+    }
+
+    // if (flag){
+    //   pcl_pub.publish(cloud_out);
+    // }
     if (flagL){
       pubL.publish(cv_ptr_l->toImageMsg());
     }
